3ab75fc1a65fb038cbb697dee02ff801a3f7372e
Norbi76 <kondora35@gmail.com>
2024-11-22 22:33:05 +0200

    Started login page

--- hsQuizGame/app/index.tsx
+++ hsQuizGame/app/index.tsx
@@ -1,9 +1,35 @@
-import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Center } from "@/components/ui/center";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField } from "@/components/ui/input";
+import { VStack } from "@/components/ui/vstack";
+// import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hei</Text>
-    </View>
+    <Center>
+      <Heading>Welcome to HearthStone QuizGame!</Heading>
+      
+      <Input variant="outline" size="md" isDisabled={false} isInvalid={false} isReadOnly={false} >
+          <InputField
+            placeholder='Enter email here...'
+          />
+      </Input>
+
+      <Input variant="outline" size="md" isDisabled={false} isInvalid={false} isReadOnly={false} >
+          <InputField
+            placeholder='Enter password here...'
+          />
+      </Input>
+      
+      <Button size="lg">
+        <ButtonText>Login</ButtonText>
+      </Button>
+
+      <Button size="md">
+        <ButtonText>Sign up</ButtonText>
+      </Button>
+    </Center>
   );
 }
